3598d2e387a7e05b04b1a30d06029c7a11044343
kpwrennbu <kpwrenn@bu.edu>
2025-06-22 18:37:34 -0400

    adds necessary models

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module rest-api-with-go
 
 go 1.24.4
+
+require (
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+)
